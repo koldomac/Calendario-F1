8ca767c9632239c6eb22df85c0fdca4ff52c2188
Koldo <kduarte@koaltechs.com>
2012-01-09 08:52:48 +0100

    v1.0

---
+++ app/app.js
@@ -0,0 +1,14 @@
+var App = (function(lng, undefined) {
+
+    //Define your LungoJS Application Instance
+    
+    lng.App.init({
+        name: 'Calendario Fórmula 1',
+        version: '1.0.0'
+    });
+    
+    return {
+
+    };
+
+})(LUNGO);
